39766ac568aafc0798f424f9b552ff89208216ec
t-Nakamura <1am1goodtime@gmail.com>
2021-06-25 14:33:55 +0000

    meshiterro2-4

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   root to: 'homes#top'
   devise_for :users
   resources :post_images, only: [:new, :create, :index, :show, :destroy]
-
+    resources :post_comments, only: [:create, :destroy]
 end
